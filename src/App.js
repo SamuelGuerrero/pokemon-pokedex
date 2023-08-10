f1cc6403ef61b71799c459e7b121d99b42595871
Samuel Guerrero <itougn15@gmail.com>
2023-08-09 18:39:48 -0600

    Added index page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import SearchPokemon from "./components/SearchPokemon";
+import PokemonCard from "./components/PokemonCard";
 
 function App() {
+  const [pokemon, setPokemon] = useState(null);
+  const [error, setError] = useState("");
+  const [isCharging, setIsCharging] = useState(false);
+
+  const getPokemon = async (pokemonName) => {
+    setIsCharging(true);
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+      .then((res) => res.json())
+      .then((data) => setPokemon(data))
+      .catch(function (error) {
+        setPokemon(null);
+        setError(error);
+      });
+
+    setIsCharging(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {!error && <SearchPokemon getPokemon={getPokemon} />}
+
+      {isCharging && <h1>Cargando...</h1>}
+      {pokemon && (
+        <div className="w-full flex justify-center mt-5">
+          <PokemonCard pokemonData={pokemon} />
+        </div>
+      )}
+      {error && (
+        <div className="mt-10">
+          <h1 className="text-xl font-bold">Error, enter the name of a valid Pokemon</h1>
+          <button onClick={() =>{
+            setError(null)
+          }} className="bg-[#ffde00] shadow-red-500 text-black font-bold py-2 px-2 mt-2 rounded-md hover:bg-red-400 hover:shadow-xl active:scale-90 transition duration-150">
+            Try again
+          </button>
+        </div>
+      )}
     </div>
   );
 }
